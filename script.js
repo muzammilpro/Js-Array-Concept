605d2fcb4848363d9eb76637a3f3dbef0f22356a
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-06 09:13:50 +0500

    New Code Added

--- script.js
+++ script.js
@@ -17,40 +17,40 @@
 
 // write a js program, that input name from user, and check is it exits in array or not?
 
-var students = [];
+// var students = [];
 
-function addUser() {
-    students[students.length] = document.getElementById("userName").value
-    printStudents()
-}
+// function addUser() {
+//     students[students.length] = document.getElementById("userName").value
+//     printStudents()
+// }
 
 
-function findUser() {
-    var searchValue = document.getElementById("searchInput").value;
-    var result = "not found";
-    for (let index = 0; index < students.length; index++) {
-        const student = students[index];
-        if (student == searchValue) {
-            result = `user found on index ${index}`
-            break;
-        }
-    }
-    document.getElementById("searchResult").innerHTML = result
-}
+// function findUser() {
+//     var searchValue = document.getElementById("searchInput").value;
+//     var result = "not found";
+//     for (let index = 0; index < students.length; index++) {
+//         const student = students[index];
+//         if (student == searchValue) {
+//             result = `user found on index ${index}`
+//             break;
+//         }
+//     }
+//     document.getElementById("searchResult").innerHTML = result
+// }
 
 
 
 // create an empty array to store values
-var myArray = [];
+// var myArray = [];
 
 // get the value from the user
-var userInput = document.getElementById("searchInput").value;
+// var userInput = document.getElementById("searchInput").value;
 
 // add the value to the array
-myArray.push(userInput);
+// myArray.push(userInput);
 
 // display the array values
-console.log(myArray);
+// console.log(myArray);
 
 
 // function printStudents() {
@@ -62,8 +62,8 @@ console.log(myArray);
 
 
 
-var studentsNames = ["Umar", "Ali", "Zain"]
-var studentsNames1 = ["Umar", "Ali", "Zain"]
+// var studentsNames = ["Umar", "Ali", "Zain"]
+// var studentsNames1 = ["Umar", "Ali", "Zain"]
 
 // studentsNames[studentsNames.length] = "Naveed"
 // studentsNames[studentsNames.length] = "Manzar"
@@ -199,3 +199,27 @@ var studentsNames1 = ["Umar", "Ali", "Zain"]
 
 
 // var userNames = ["a", "b","c","e"]
+
+
+
+var users = [];
+
+		function addUser() {
+			var name = document.getElementById("userName").value;
+			users.unshift(name);
+			document.getElementById("userName").value = "";
+			console.log(users);
+		}
+
+		function findUser() {
+			var searchValue = document.getElementById("searchInput").value;
+			var result = "User not found.";
+			for (let i = 0; i < users.length; i++) {
+				if (users[i] === searchValue) {
+					result = `User found at index ${i}.`;
+					break;
+				}
+			}
+			document.getElementById("searchResult").innerHTML = result;
+			document.getElementById("searchInput").value = "";
+		}
