8ea439f35fe3470f05d92b0d0a7a5027addefcc1
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-14 15:13:10 +0500

    JS Added

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var user = []
+var userData = []
 
 function Submit(){
-    var 
+    var username = document.getElementById("Name").value
+    var useremail = document.getElementById("Email").value
+    var userPhoneNumber = document.getElementById("phoneNumber").value
 
+    var inputData {
+        Name : username,
+        Email: useremail,
+        PhoneNumber: userPhoneNumber
+    }
+    userData.push(inputData)
+
+}
+
+function displayData(){
+    
 }
