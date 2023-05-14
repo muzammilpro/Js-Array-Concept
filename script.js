3bd061edfcd320a20411fa4b683b2df5afef7117
muzammilpro <muzammilhusnain7@gmail.com>
2023-05-14 16:09:59 +0500

    Fix errors

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-var userData = []
+
+var userData = [];
 
 function Submit() {
-    var username = document.getElementById("Name").value
-    var useremail = document.getElementById("Email").value
-    var userPhoneNumber = document.getElementById("phoneNumber").value
+    var username = document.getElementById("Name").value;
+    var useremail = document.getElementById("Email").value;
+    var userPhoneNumber = document.getElementById("phoneNumber").value;
 
-    var inputData {
-        Name: username,
-        Email: useremail,
-        PhoneNumber: userPhoneNumber
-    }
-    userData.push(inputData)
-
+    var inputData = {
+        name: username,
+        email: useremail,
+        phone: userPhoneNumber
+    };
+    userData.push(inputData);
+    displayData();
 }
 
 function displayData() {
+    var toDisplay = "toDisplay";
+    var displayDataHTML = "";
 
+    for (let index = 0; index < userData.length; index++) {
+        var currentData = userData[index];
+        displayDataHTML += `
+                    <div>
+                        <h1>Name: ${currentData.name}</h1>
+                        <p>Email: ${currentData.email}</p>
+                        <span>Phone: ${currentData.phone}</span>
+                    </div>
+                `;
+    }
 
+    document.getElementById(toDisplay).innerHTML = displayDataHTML;
 }
